71c568454ab4daa0888b6b149c936cddc0899693
Gerardo Ritchey <gerardoritchey@gmail.com>
2023-09-25 18:03:02 -0700

    Updates
    updates main.py to include OAuth implementation for Instagram
    
    Updates requirements to include requests and Authlib
    
    Fixes base html and head html templates to correctly place <head> within the HTML structure.
    -- Uses Jinja and Flask templating to return the correct uri for static assets, rather than relying on relative paths.

--- main.py
+++ main.py
@@ -1,28 +1,79 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from datetime import date
-import os
+import os, requests
+from authlib.integrations.flask_client import OAuth
 
+
+app = Flask(__name__)
+app.secret_key = 'h@mburger5'
+Bootstrap5(app)
 
 host = os.environ.get("FLASK_HOST")
 port = os.environ.get("FLASK_PORT")
 
-app = Flask(__name__)
-Bootstrap5(app)
+fb_client_id = os.environ.get("FB_APP_ID")
+fb_secret = os.environ.get("FB_APP_SECRET")
+
+oauth = OAuth(app)
+
+# Instagram OAuth Configuration
+oauth.register(
+    name='instagram',
+    client_id= fb_client_id,
+    client_secret= fb_secret, 
+    authorize_url='https://api.instagram.com/oauth/authorize',
+    authorize_params=None,
+    authorize_params_callback=None,
+    authorize_url_params=None,
+    fetch_token='https://api.instagram.com/oauth/access_token',
+    fetch_token_params=None,
+    fetch_token_method='POST',
+    client_kwargs={'scope': 'user_profile,user_media'},
+)
+
+
 
 
 @app.route('/')
 def home():
 
-
-
     return render_template("home.html")
 
 
+@app.route("/insta/")
+def instagram_imgs():
+
+
+    return render_template("instagram.html")
+
+
+@app.route("/login")
+def login():
+    redirect_uri = url_for('authorize', _external=True)
+
+    return oauth.instagram.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.instagram.authorize_access_token()
+    session['token'] = token
+    return redirect(url_for('profile'))
+
+
+# Display Instagram Profile
+@app.route('/profile')
+def profile():
+    token = session.get('token')
+    if token is None:
+        return 'Not authenticated.'
+    user_info = oauth.instagram.get('me', token=token)
+    return f'Instagram User ID: {user_info["id"]}, Username: {user_info["username"]}'
+
 
 if __name__ == "__main__":
-    app.run(host=host, port=port, debug=False)
+    app.run(host=host, port=port, debug=True)
