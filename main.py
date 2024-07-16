886ed328625e6769c932b1f334ab2e6acad25cf9
Gerardo Ritchey <gerardoritchey@gmail.com>
2024-07-16 16:12:21 -0700

    Instagram API removal
    *comments out the section that queries IG until a new API key can be generated
    
    *updates to the 2024 timeline.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
-from social_apis.instagram import Instagram
+#from social_apis.instagram import Instagram
 from datetime import date
 import sys, os, requests
 from dotenv import load_dotenv
@@ -24,11 +24,11 @@ fb_client_id = os.environ.get("FB_APP_ID")
 fb_secret = os.environ.get("FB_APP_SECRET")
 far_llat = os.environ.get("IG_FAR_LLAT")
 
-ig = Instagram(
-    client_id=fb_client_id,
-    client_secret=fb_secret,
-    ll_at=far_llat
-)
+# ig = Instagram(
+#     client_id=fb_client_id,
+#     client_secret=fb_secret,
+#     ll_at=far_llat
+# )
 
 
 @app.route('/')
@@ -36,17 +36,17 @@ def home():
 
     #the IG api right now does not allow us to query specific types of content, so we pull it all, and filter it here
     #todo add a method to the IG object that returns only the data type we want. Could be helpful for later
-    user_media_data = ig.get_user_media(limit=10)
+    # user_media_data = ig.get_user_media(limit=10)
     user_images = []
 
-    for media in user_media_data["data"]:
-        if media["media_type"] == "IMAGE":
-            user_images.append(media)
-        elif media["media_type"] == "CAROUSEL_ALBUM":
-            caption = media["caption"]
-            for image in media["children"]["data"]:
-                image["caption"] = caption  #IG child edges don't have captions, we need to use the ALBUM'S CAPTIONS
-                user_images.append(image)
+    # for media in user_media_data["data"]:
+    #     if media["media_type"] == "IMAGE":
+    #         user_images.append(media)
+    #     elif media["media_type"] == "CAROUSEL_ALBUM":
+    #         caption = media["caption"]
+    #         for image in media["children"]["data"]:
+    #             image["caption"] = caption  #IG child edges don't have captions, we need to use the ALBUM'S CAPTIONS
+    #             user_images.append(image)
     
 
     user_images = user_images[:(len(user_images) %4 * -1)] #get a full block on the home page
@@ -54,17 +54,17 @@ def home():
     return render_template("home.html", ig_images=user_images)
 
 
-@app.route("/insta/")
-def instagram_imgs():
-
-    user_media_list = ig.get_user_media(limit=15)
-
-    #filter down to just images until we sort out how to handle videos
-    user_images = [ media for media in user_media_list["data"] if media["media_type"] != "VIDEO"]
-
-    return render_template("instagram.html", ig_media=user_images)
-
-
+# @app.route("/insta/")
+# def instagram_imgs():
+#
+#     user_media_list = ig.get_user_media(limit=15)
+#
+#     #filter down to just images until we sort out how to handle videos
+#     user_images = [ media for media in user_media_list["data"] if media["media_type"] != "VIDEO"]
+#
+#     return render_template("instagram.html", ig_media=user_images)
+#
+#
 
 
 
